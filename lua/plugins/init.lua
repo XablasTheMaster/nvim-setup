d7e4c3bffe53d3fbda4e961e755597206898c48c
XablasTheMaster <n1c0l451lv4.32@gmail.com>
2026-02-08 12:17:15 -0300

    more

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,4 @@
+
 return {
     {
         "stevearc/conform.nvim",
@@ -166,29 +167,28 @@ return {
         opts = {},
    },
 
-   {
-       {
-          'mrcjkb/rustaceanvim',
-          version = '^7', -- Recommended
-          lazy = false, -- This plugin is already lazy
-          config = function()
-            local mason_registry = require('mason-registry')
-            local codelldb = mason_registry.get_package("codelldb")
-            local extension_path = codelldb:get_install_path() .. "/extension/"
-            local codelldb_path = extension_path .. "adapter/codelldb"
-            -- local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
-            -- If you are on Linux, replace the line above with the line below:
-            local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-            local cfg = require('rustaceanvim.config')
+     {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    ft = "rust",
+    config = function ()
+      --local mason_registry = require('mason-registry')
+      --local codelldb = mason_registry.get_package("codelldb")
+   --   local extension_path = codelldb:get_install_path() .. "/extension/"
+      --local codelldb_path = extension_path .. "adapter/codelldb"
+   --   local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
+	-- If you are on Linux, replace the line above with the line below:
+	 --local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      --local cfg = require('rustaceanvim.config')
 
-            vim.g.rustaceanvim = {
-               dap = {
-                  adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-               },
-            }
-          end
-       }
-   },
+      --vim.g.rustaceanvim = {
+        --dap = {
+         -- adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+       -- },
+     -- }
+     end
+  },
 
    {
         'rust-lang/rust.vim',
@@ -243,8 +243,7 @@ return {
     end
   },
 
-
-    -- test new blink
+   -- test new blink
     -- { import = "nvchad.blink.lazyspec" },
 
     -- {
